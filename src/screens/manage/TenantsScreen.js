1e85069b0ee7d19785624751d266262fd1ae95cd
tuongnguyen <nctuong99@gmail.com>
2022-01-02 20:43:57 +0700

    show tenants

--- src/screens/manage/TenantsScreen.js
+++ src/screens/manage/TenantsScreen.js
@@ -1,17 +1,41 @@
 import { useNavigation, useRoute } from '@react-navigation/native';
-import React from 'react';
-import { View, StyleSheet, Alert } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, StyleSheet, Alert, Text, FlatList } from 'react-native';
+import { List, Surface } from 'react-native-paper';
+import { useDispatch, useSelector } from 'react-redux';
 import { AppBar } from '../../components';
 import { FAB } from '../../components/common';
 import { CONTRACT, TENANT_EDIT } from '../../constants/navigation';
+import { getTenants } from '../../store/slices/tenantSlice';
 
 function TenantScreen(props) {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
   const handleCreatePress = () => navigation.navigate(TENANT_EDIT);
+  const { tenants, loading } = useSelector(state => state.tenant);
+
+  useEffect(() => {
+    dispatch(getTenants());
+  }, []);
 
   return (
     <View style={styles.container}>
       <AppBar title='Người thuê' />
+      <Surface style={styles.contentContainer}>
+        <FlatList
+          data={tenants}
+          keyExtractor={item => item.renterId}
+          renderItem={({ item }) => {
+            return (
+              <List.Item
+                title={item.name}
+                description={item.phone}
+                left={props => <List.Icon {...props} icon='account' />}
+              />
+            );
+          }}
+        />
+      </Surface>
       <FAB onPress={handleCreatePress} />
     </View>
   );
@@ -19,6 +43,9 @@ function TenantScreen(props) {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  contentContainer: {
     flex: 1,
   },
 });
--- src/store/slices/tenantSlice.js
+++ src/store/slices/tenantSlice.js
@@ -1,9 +1,14 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { createTenant as createTenantAPI } from '../../api/tenant';
+import { createTenant as createTenantAPI, fetchTenants } from '../../api/tenant';
 
 const createTenant = createAsyncThunk('tenant/create', async params => {
   const { tenant } = params;
   await createTenantAPI(tenant);
+});
+
+const getTenants = createAsyncThunk('tenant/getAll', async () => {
+  const data = await fetchTenants();
+  return data;
 });
 
 const tenantSlice = createSlice({
@@ -26,8 +31,22 @@ const tenantSlice = createSlice({
       state.loading = false;
       state.error = action.payload;
     });
+
+    builder.addCase(getTenants.pending, (state, action) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(getTenants.fulfilled, (state, action) => {
+      state.loading = false;
+      state.error = null;
+      state.tenants = action.payload;
+    });
+    builder.addCase(getTenants.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    });
   },
 });
 
-export { createTenant };
+export { createTenant, getTenants };
 export default tenantSlice.reducer;
