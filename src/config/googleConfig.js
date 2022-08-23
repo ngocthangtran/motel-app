ad859bcbf6dec2bf15f6858dce039496d5709d62
thang.tn <ngocthangtrantb@gmail.com>
2022-08-23 23:26:32 +0700

    fix auth sdk 45

--- src/config/googleConfig.js
+++ src/config/googleConfig.js
@@ -2,12 +2,14 @@ import {
   IOS_CLIENT_ID,
   ANDROID_CLIENT_ID,
   ANDROID_STANDALONE_APP_CLIENT_ID,
+  EXPO_CLIENT_ID
 } from '@env';
 
 export const googleLoginCfg = {
   iosClientId: IOS_CLIENT_ID,
   androidClientId: ANDROID_CLIENT_ID,
-  androidStandaloneAppClientId: ANDROID_STANDALONE_APP_CLIENT_ID,
-  // behavior: 'web',
-  scopes: ['profile', 'email'],
+  expoClientId:EXPO_CLIENT_ID,
 };
+// webClientId: ANDROID_STANDALONE_APP_CLIENT_ID,
+// // behavior: 'web',
+// scopes: ['profile', 'email'],
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import * as Google from 'expo-google-app-auth';
+import React, { useEffect } from 'react';
+// import * as Google from 'expo-google-app-auth';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button, Snackbar, Surface } from 'react-native-paper';
 import { login } from '../store/slices/authSlice';
@@ -9,39 +9,63 @@ import { useNavigation } from '@react-navigation/core';
 import { googleLoginCfg } from '../config/googleConfig';
 import useDisclosure from '../hooks/useDisclosure';
 
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+
+WebBrowser.maybeCompleteAuthSession();
+
 function LoginScreen(props) {
-  const [errorVisible, showError, dismissError] = useDisclosure();
-  const dispatch = useDispatch();
   const navigation = useNavigation();
-  const { user, error } = useSelector(state => state.auth);
+  const [request, response, promptAsync] = Google.useAuthRequest(googleLoginCfg);
+  const dispatch = useDispatch();
 
-  // console.log(googleLoginCfg);
-
-  const handleGoogleLogin = async () => {
-    try {
-      const { idToken } = await Google.logInAsync(googleLoginCfg);
-      dispatch(login({ token: idToken, provider: 'Google' }));
-      await new Promise(res => setTimeout(res, 1000));
-      if (error) showError();
-      else navigation.goBack();
-    } catch (error) {
-      alert(error.message);
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { authentication } = response;
+      console.log(authentication.accessToken)
+      dispatch(login({ token: authentication.accessToken, provider: 'Google' }));
+      // await new Promise(res => setTimeout(res, 1000));
+      // if (error) showError();
+      // else navigation.goBack();
     }
-  };
+  }, [response]);
+  // const [errorVisible, showError, dismissError] = useDisclosure();
+  // const { user, error } = useSelector(state => state.auth);
+
+  // // console.log(googleLoginCfg);
+
+  // const handleGoogleLogin = async () => {
+  //   try {
+  //     const { idToken } = await Google.logInAsync(googleLoginCfg);
+  //     dispatch(login({ token: idToken, provider: 'Google' }));
+  //     await new Promise(res => setTimeout(res, 1000));
+  //     if (error) showError();
+  //     else navigation.goBack();
+  //   } catch (error) {
+  //     alert(error.message);
+  //   }
+  // };
 
   const handleBackButtonPress = () => navigation.goBack();
 
   return (
     <SafeAreaContainer>
       <Surface style={styles.container}>
-        <Button icon='google' onPress={handleGoogleLogin} mode='outlined'>
+        <Button 
+          icon='google' 
+          onPress={() => {
+            promptAsync();
+          }}
+          mode='outlined'
+          disabled={!request}
+        >
           Login with Google
         </Button>
         <Button icon='chevron-left' onPress={handleBackButtonPress}>
           Back
         </Button>
       </Surface>
-      <Snackbar
+      {/* <Snackbar
         visible={errorVisible}
         onDismiss={dismissError}
         action={{
@@ -52,7 +76,7 @@ function LoginScreen(props) {
         }}
       >
         Hey there! I'm a Snackbar.
-      </Snackbar>
+      </Snackbar> */}
     </SafeAreaContainer>
   );
 }
